4014d41068e739b0d6588997379ec396f3e73ebc
jjenus <biz.jenus@gmail.com>
2022-07-16 06:18:51 +0100

    updated print.c

--- _printf.c
+++ _printf.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_print_specifier - checks if there is a valid format specifier
- * @format: input format specifier
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
  *
  * Return: pointer to valid function or NULL
  */
-int (*get_print_specifier(const char *format))(va_list)
+static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
@@ -37,132 +37,45 @@ int (*get_print_specifier(const char *format))(va_list)
 }
 
 /**
- * _printf - Prints data according to the supplied format
- * @format: data to print
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
  *
- * Return: count of printed characters
+ * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
-	int (*args_ptr)(va_list);
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	va_start(args, format);
-
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
 	while (format[i])
 	{
 		for (; format[i] != '%' && format[i]; i++)
 		{
-			count++;
 			_putchar(format[i]);
+			count++;
 		}
-		
 		if (!format[i])
 			return (count);
-		
-		args_ptr = get_print_specifier(&format[i + 1]);
-		
-		if (args_ptr != NULL)
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
 		{
-			count += args_ptr(args);
+			count += f(valist);
 			i += 2;
 			continue;
 		}
-		
 		if (!format[i + 1])
 			return (-1);
-		
 		_putchar(format[i]);
 		count++;
-
 		if (format[i + 1] == '%')
 			i += 2;
 		else
 			i++;
-		
 	}
-
-	va_end(args);
-
-	return (count);
-}
-
-/**
- * print_i - prints an integer
- * @i: integer to print
- *
- * Return: number of chars and digits printed
- */
-int print_i(va_list i)
-{
-	int a[10];
-	int j, m, n, sum, count;
-
-	n = va_arg(i, int);
-	count = 0;
-	m = 1000000000;
-	a[0] = n / m;
-	for (j = 1; j < 10; j++)
-	{
-		m /= 10;
-		a[j] = (n / m) % 10;
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		for (j = 0; j < 10; j++)
-			a[j] *= -1;
-	}
-	for (j = 0, sum = 0; j < 10; j++)
-	{
-		sum += a[j];
-		if (sum != 0 || j == 9)
-		{
-			_putchar('0' + a[j]);
-			count++;
-		}
-	}
-	return (count);
-}
-
-/**
- * print_d - prints a decimal
- * @d: decimal to print
- *
- * Return: number of chars and digits printed
- */
-int print_d(va_list d)
-{
-	int a[10];
-	int j, m, n, sum, count;
-
-	n = va_arg(d, int);
-	count = 0;
-	m = 1000000000;
-	a[0] = n / m;
-	for (j = 1; j < 10; j++)
-	{
-		m /= 10;
-		a[j] = (n / m) % 10;
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		for (j = 0; j < 10; j++)
-			a[j] *= -1;
-	}
-	for (j = 0, sum = 0; j < 10; j++)
-	{
-		sum += a[j];
-		if (sum != 0 || j == 9)
-		{
-			_putchar('0' + a[j]);
-			count++;
-		}
-	}
+	va_end(valist);
 	return (count);
 }
